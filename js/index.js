9f37ae99f0b0e7b25cbc301afeedfc470cf3836b
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2024-12-09 14:29:28 +0100

    chg content in displayed card in listings

--- js/index.js
+++ js/index.js
@@ -6,9 +6,6 @@ const API_LOGIN = "/login";
 const API_AUCTION = "/auction";
 const API_LISTINGS = "/listings";
 
-//display all listings - no auth required
-
-
 
 //fetch data from the API
  async function getListings() {
@@ -18,39 +15,46 @@ const API_LISTINGS = "/listings";
 }
 getListings();
 
-// get listings as an array
-// async function getListingsData() {
-// 	const listings = await getListings();
-// 	console.log(listings.data);
-//  };
-
-//  getListingsData();
-
+//display listings on the page - no auth required
  async function displayListings() {
 	const listings = await getListings();
 	const data = listings.data;
+
+
+	 // Sort the data by created date (if not already sorted) and display only the 20 latest
+	 const latestListings = data
+	 .sort((a, b) => new Date(b.created) - new Date(a.created)) 
+	 .slice(0, 20);
    
 	const feedContainer = document.getElementById("display-listings-container");
    
-	const listItem = data.map((el) => {
+	const listItem = latestListings.map((el) => {
 	 const title = el.title;
-	 const description = el.description;
 	 const id = el.id;
+	 const description = el.description;
 	 const imageURL = el.media && el.media.url ? el.media.url : "https://images.unsplash.com/photo-1558293842-c0fd3db86157?q=80&w=2929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+	 const countBids = el._count.bids
 	 const created = el.created;
 	 const endsAt = el.endsAt;
 
 
-	 return `<div class="card shadow mt-5 mx-3 mb-3 col-md-8 col-lg-6 col-xl-8">
+	 return `<div class="card shadow col-md-6 col-lg-4 col-xl-3 col-xxl-3 mb-4">
 	 <div class="card-body row">
-		 <p>${title}</p>
-			 <p>${id}</p>
+		 <h1>${title}</h1>
 			 <p>${description}</p>
 			 <img src=${imageURL} />
 			 <p>Posted: ${created}</p>
 			 <p>Auction ends: ${endsAt}</p>
-	 </div>
-   </div>`;
+			 <p>Bids: ${countBids}</p>
+			  <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+				   data-bs-target="#staticBackdrop">
+				   View item
+			   </button>
+	 </div> 	
+   </div> `
+
+ 
+   ;
 	});
 
 	const listItemsHTML = listItem.join('');
@@ -59,3 +63,6 @@ getListings();
   };
 
   displayListings();
+
+  //Search function
+
